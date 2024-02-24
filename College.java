7622d74d02e864064ecd777b2e62246401607744
manikandanmk7305 <manikandanmk7305@gmail.com>
2024-02-24 21:41:32 +0530

    mk

---
+++ College.java
@@ -0,0 +1,10 @@
+public class College {
+    public static void main(String[] args){
+     int length=50,breadth=20;
+        int a,b;
+        a=2*(length+breadth);
+        b=length*breadth;
+        System.out.println(a+"ms");
+        System.out.println(b+"sqm");
+    }
+}
